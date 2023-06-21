704095e16d49ff272476450088ba5b30587036bf
easyscripter <deniscs49@gmail.com>
2023-06-21 14:50:42 +0300

    feat: add drag areas

--- src/pages/Panel/Panel.tsx
+++ src/pages/Panel/Panel.tsx
@@ -5,10 +5,24 @@ import { MdOutlineSettings } from 'react-icons/md';
 const Panel = () => {
     const [isShowWidgets, setIsShowWidgets] = useState(false);
     const onclick = () => {
-        setIsShowWidgets(true);
+        setIsShowWidgets((prevState) => !prevState);
     }
     return ( 
         <div className={style.container}>
+            <div className={style.dragsContainer}>
+                <div className={style.dragContainer}>
+                    area 1
+                </div>
+                <div className={style.dragContainer}>
+                    area 2
+                </div>
+                <div className={style.dragContainer}>
+                    area 3
+                </div>
+                <div className={style.dragContainer}>
+                    area 4
+                </div>
+            </div>
             <div className={style.widgetListContainer}>
                 <MdOutlineSettings onClick={onclick} className={style.widgetIcon} size={25}/>
                 <WidgetList isShow={isShowWidgets}>
